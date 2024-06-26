23d101b7472ac2c5ba4d5c4cbfe14f993d2610ed
bechirMakhlouf <bechirM@proton.me>
2024-06-26 20:55:49 +0100

    add configuration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+mod configuration;
+mod routes;
 use std::net::TcpListener;
 use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind to a random port");
+    let _configuration = configuration::get_configuration().unwrap();
+
     println!("running on port: {}", listener.local_addr().unwrap().port());
     let _ = run(listener)?.await;
     Ok(())
