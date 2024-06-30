db91d81d392ec3c94d247271ea7d933ae90505d4
bechirMakhlouf <bechirM@proton.me>
2024-06-30 19:32:22 +0100

    add hierarchical configuration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod configuration;
 mod routes;
 mod telemetry;
+use configuration::Settings;
 use std::net::TcpListener;
 use telemetry::init_subscriber;
 use zero2prod::run;
@@ -10,15 +11,20 @@ async fn main() -> Result<(), std::io::Error> {
     let subscriber = telemetry::get_subscriber("into".into(), "debug".into());
     init_subscriber(subscriber);
 
-    let configuration = configuration::get_configuration().unwrap();
-    let server_url = format!("127.0.0.1:{}", &configuration.application_port);
+    let Settings {
+        application,
+        database: _,
+    } = configuration::get_configuration().unwrap();
+
+    let server_url = format!("{}:{}", application.host, application.port);
     let listener = TcpListener::bind(server_url).unwrap();
+
     let connection_string = configuration::get_configuration()
         .expect("failed to get configuration")
         .database
         .connection_string();
-    let db_pool = sqlx::postgres::PgPool::connect(&connection_string)
-        .await
+
+    let db_pool = sqlx::postgres::PgPool::connect_lazy(&connection_string)
         .expect("failed to connect to postgres database");
 
     println!("running on port: {}", listener.local_addr().unwrap().port());
